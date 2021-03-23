daa00ec35dfabf89b52610f20ee826684bfedec8
Leidy <paoalm1@hotmail.com>
2021-03-23 13:20:25 -0500

    trayendo nombres prueba 1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
 // import data from './data/pokemon/pokemon.js';
+
+
+// console.log(example, data);
+
 import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//  console.log(data.results);
+
+ let Informacion = data.results;
+
+ for (let i=0;i < Informacion.length;i++){
+     let p = Informacion[i].name;
+     console.log(p);
+ }
+
+//  let Personaje =  Informacion.filter(function(Genero) {
+//     return Genero.gender == "male";
+    
+
+
+
+
+
+// for (var i = 0, max = dataresults.length; i < max; i += 1) {
+ 
+//     dataresults.push(data[i].name);
+ 
+
+//   console.log(dataresults)
+ 
+
+
+
+ 
