e5b6c67f64e186ce6e5f8b756b10e1e952b52973
unknown <lorena0717@hotmail.com>
2021-03-25 08:53:03 -0500

    html fichas, llamado data

--- src/main.js
+++ src/main.js
@@ -2,42 +2,74 @@ import { example } from './data.js';
 
 import data from './data/rickandmorty/rickandmorty.js';
 
-//  console.log(data.results);
+const arrayPersonajes=data.results// en esta variable guardo la data ingresando a results donde esta el array
+//console.log(arrayPersonajes)
 
- const Informacion = data.results;
- const nombrePersonajes=Informacion.sort((personajes1,personajes2)=>{
- return (personajes1.name<personajes2.name)? -1 :1//
- })
-//console.log(nombrePersonajes) a-z
+let Busqueda = document.getElementById("input-buscador");
 
- const status=Informacion.filter(status=> {
-     return status!=="Dead"
- })
-// console.log(status)
+console.log(Busqueda.value)
+
+
+// let nuevaTarjeta=document.createElement("div");
+// nuevaTarjeta.classList.add('tarjeta')
+// document.body.appendChild(nuevaTarjeta);
+// }
+
+let nombrePersonajedata="";
+let nombrePersonaje=document.getElementById("nombrePersonaje");
+for(let i=0;i < arrayPersonajes.length; i++){
+   let nombresPersonajes=arrayPersonajes[i].name;
+   nombrePersonajedata=nombrePersonajedata+nombresPersonajes+"\n";
+}
+//console.log(nombrePersonajedata);
+//nombrePersonaje.innerHTML=nombrePersonajedata;
  
- for (let i=0;i < Informacion.length;i++){
-     let categoriaNombre = Informacion[i].name;
-     console.log(categoriaNombre);
+ 
+let imgPersonajeData="";
+let fotoImg=document.getElementById("imgPersonaje");
+  for(let i=0; i < arrayPersonajes.length; i++){
+      let imgPersonajes=arrayPersonajes[i].image;
+      imgPersonajeData=imgPersonajeData+imgPersonajes;
  }
+ //console.log(imgPersonajeData);
+ //fotoImg.innerHTML=imgPersonajeData;
 
- for (let i=0;i < Informacion.length;i++){
-     //let categoriaGenero = Informacion[i].gender;
-
-
-    //console.log(categoriaGenero);
-    }
-
- for (let i=0;i < Informacion.length;i++){
-    let categoriaEspecie = Informacion[i].species;
-    //console.log(categoriaEspecie);
-    }
+ 
+ const nombrePersonajes=arrayPersonajes.sort((personajes1,personajes2)=>{// traemos todos los nombres de los personajes y se organizan con sort 
+ return (personajes1.name<personajes2.name)? -1 :1 //operador ternario
+ })
+ //console.log(nombrePersonajes) // esta consola imprime los nombre organizados de la a-z
 
 
-//  let Personaje =  Informacion.filter(function(Genero) {
-//     return Genero.gender == "male";
-    
+
+// function TarjetasVisibles(){
+//     document.getElementById("contenedorTarjetas").style.display="block"
+// }
+ 
 
 
+
+ const status=arrayPersonajes.filter(status=> {// con filter traemos los datos especificos (aun no filtra correctamente)
+     return status !="Dead"
+ })
+  //console.log(status)
+ 
+// for (let i=0;i <nombrePersonajes.lenght;i++){
+//      let contenedorPersonajes=document.getElementById("personajes");
+//      let nuevoElemento=document.getElementById("div");
+//      let nuevaImagen=document.getElementById("imgPersonaje");
+//      let nuevoNombre=document.getElementById("nombrePersonaje").textContent="Nombre";
+
+//      nuevoElemento.className="tarjeta";
+
+//      nuevaImagen.src=personajes[i].image;
+//      nuevoNombre.innerHTML=personajes[i].name;
+
+//      nuevoElemento.appendChild(nuevaImagen); 
+//      nuevoElemento.appendChild(nuevoNombre);
+//      contenedorPersonajes.appendChild(nuevoElemento);
+
+// }
  
 
 
