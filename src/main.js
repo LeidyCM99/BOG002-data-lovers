46470302629377deebb7624039246014db1a1943
Leidy <paoalm1@hotmail.com>
2021-03-25 08:54:02 -0500

    funciones con click

--- src/main.js
+++ src/main.js
@@ -1,34 +1,96 @@
 import { example } from './data.js';
 
 import data from './data/rickandmorty/rickandmorty.js';
+let Data = data.results;
 
-//  console.log(data.results);
 
- const Informacion = data.results;
- const nombrePersonajes=Informacion.sort((personajes1,personajes2)=>{
+//***************************************** Boton de busqueda*****************************************
+
+const Busqueda = document.getElementById("input-buscador");
+const Boton = document.getElementById("Buscar");
+
+Boton.addEventListener("click",BuscandoDEntroDeData)
+   
+
+function BuscandoDEntroDeData(){
+const Resultado = Busqueda.value;
+//     for(let TodaLaData of Data){
+//         let SacarData = TodaLaData.name;
+
+//         if (SacarLaData.indexOf(Resultado)!==-1){
+//          console.log(SacarData)
+//         }}}
+            
+           
+
+//  for(let datos of Data ){
+//         let nombre = datos.name;
+//         if (nombre.indexOf(Resultado)!==-1){
+            
+//         }
+//     }
+
+    console.log(Resultado)}
+
+// *****************************************Lista de links*****************************************
+
+let Episodios = document.getElementById("Episodios");
+Episodios.addEventListener("click",
+function(){
+         let Data = data.results;
+    for (let i=0;i < Data.length;i++){
+        let episode = Data[i].episode;
+        let ArrEpisodios = [episode];
+        let ArrNuevo = []
+    
+        ArrEpisodios.forEach (el => {
+             if (!(el in Data)) {// comprobamos si el valor existe en el objeto
+                Data[el] = true; // si no existe creamos ese valor y lo añadimos al array final, y si sí existe no lo añadimos
+                ArrNuevo.push(el);
+  }
+})
+// document.getElementById("ParrafoDePrueba").innerHTML +=  ArrNuevo;
+// console.log(ArrEpisodios) 
+console.log(ArrNuevo.toString()) 
+    
+}})
+
+
+//******************** */ RESULTADOS DE DATA SEGUN NOMBRE, GENERO, ETC ************************************
+//  
+
+//  let Data = data.results;
+ const nombrePersonajes=Data.sort((personajes1,personajes2)=>{
  return (personajes1.name<personajes2.name)? -1 :1//
  })
 //console.log(nombrePersonajes) a-z
 
- const status=Informacion.filter(status=> {
+ const status=Data.filter(status=> {
      return status!=="Dead"
  })
 // console.log(status)
  
- for (let i=0;i < Informacion.length;i++){
-     let categoriaNombre = Informacion[i].name;
-     console.log(categoriaNombre);
- }
+let NombreDePersonajes = document.getElementById("ListaPersonajes");
+NombreDePersonajes.addEventListener("click",
+function(){
+ for (let i=0;i < Data.length;i++){
+     let categoriaNombre = Data[i].name;
 
- for (let i=0;i < Informacion.length;i++){
+     console.log(categoriaNombre)}
+
+    })
+
+
+
+//  for (let i=0;i < Data.length;i++){
      //let categoriaGenero = Informacion[i].gender;
 
 
     //console.log(categoriaGenero);
-    }
+    // }
 
- for (let i=0;i < Informacion.length;i++){
-    let categoriaEspecie = Informacion[i].species;
+ for (let i=0;i < Data.length;i++){
+    let categoriaEspecie = Data[i].species;
     //console.log(categoriaEspecie);
     }
 
@@ -40,6 +102,3 @@ import data from './data/rickandmorty/rickandmorty.js';
 
  
 
-
-
- 
