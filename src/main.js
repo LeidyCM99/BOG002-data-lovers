ec2cefecb3c1ab585c6d92f78381cdf15cfdcf71
Leidy <paoalm1@hotmail.com>
2021-04-05 08:32:46 -0500

    cambios con filtro

--- src/main.js
+++ src/main.js
@@ -24,8 +24,6 @@ function BuscandoDEntroDeData(){//ejecutamos la función Busqueda
     for (let i=0;i < Data.length;i++){
       if(Tarjeta[i].textContent.toUpperCase().includes(Resultado)) {
         Tarjeta[i].style.display = "block";
-       
-
         
       } else {
         Tarjeta[i].style.display = "none";
@@ -34,18 +32,17 @@ function BuscandoDEntroDeData(){//ejecutamos la función Busqueda
   }
 
 
-
 // *****************************************CREACION DE TARJETAS*****************************************
 
 let nombrePersonajedata="";
-//let nombrePersonaje=document.getElementById("contenedorTarjetas")
+
 
 for(let i=0;i < Data.length; i++){////esta función recorre la data
-   const Nombres=Data[i].name;//accede a los datos  clasificados en el array en este caso name y los guarda en la nueva variable
-   const Origen=Data[i].origin.name;
-   const Genero=Data[i].gender;
-   const Episodios=Data[i].episode.length;
-   const imagen=Data[i].image;
+   const Nombres   = Data[i].name;//accede a los datos  clasificados en el array en este caso name y los guarda en la nueva variable
+   const Origen    = Data[i].origin.name;
+   const Genero    = Data[i].gender;
+   const Episodios = Data[i].episode.length;
+   const imagen    = Data[i].image;
    clonar(Nombres, Origen, Genero, imagen, Episodios) //ejecutamos la funcion clonar con los datos del parametro
    
 }
@@ -55,91 +52,89 @@ nombrePersonaje.textContent=nombrePersonajedata;
 
 function clonar(Nombres, Origen, Genero, Imagen, Episodios) {//Creamos esta función declarando los parametros a usar
   var contenedor = document.getElementById("tarjeta");//reservamos un id en html
-  var clon = contenedor.cloneNode(true);//a contenedor le aplicamos el metodo cloneNode para clonar
+  var clon       = contenedor.cloneNode(true);//a contenedor le aplicamos el metodo cloneNode para clonar
+
   clon.style.display= "inline-flex";//aplicamos un estilo display y una margen para los nuevos div´s
-  clon.style.margin= "30px"        
+  clon.style.margin= "20px"        
   
   let nombrePersonaje=document.getElementById("nombrePersonaje");//reservamos un id en html
   nombrePersonaje.innerHTML = Nombres//imprimimos el dato requerido
             
   let lugar_personaje=document.getElementById("lugarDeOrigen");
-  lugar_personaje.innerHTML = "Lugar De Origen: "+Origen
+  lugar_personaje.innerHTML = "Lugar De Origen: "+ Origen
                    
   let cantidad_episodios=document.getElementById("cantidadDeDeEpisodios");
-  cantidad_episodios.innerHTML = "Cantidad de episodios: "+Episodios
+  cantidad_episodios.innerHTML = "Cantidad de episodios: "+ Episodios
   
   let genero_tarjeta=document.getElementById("genero");
-  genero_tarjeta.innerHTML = "Genero: "+Genero
+  genero_tarjeta.innerHTML = "Genero: "+ Genero
 
   document.getElementById("imgPersonaje").src = Imagen
 
   let contenedorDeTarjetas=document.getElementById("contenedorTarjetas")
   contenedorDeTarjetas.appendChild(clon);//crea un nuevo nodo con el formato que le dimos
-    
   }
 
-// OCULTAR Y MOSTRAR SECCIONES ***********
+// ***********OCULTAR Y MOSTRAR SECCIONES ***********
 
 
 let QuienesSomos=  document.getElementById("Somos");
     QuienesSomos.addEventListener("click",function (){
           document.getElementById("contenedorTarjetas").style.display = "none";
-          document.getElementById("SomosQ").style.display = "block"; 
-          document.getElementById("aside").style.display = "none"; 
-          
-         
+          document.getElementById("SomosQ").style.display = "block";  
+          document.getElementById("filtroAZ").style.display="none";    
   })
 
   let Home=  document.getElementById("Home");
     Home.addEventListener("click",function (){
           document.getElementById("contenedorTarjetas").style.display = "block";
           document.getElementById("SomosQ").style.display = "none"; 
-          document.getElementById("aside").style.display = "block"; 
+          document.getElementById("filtroAZ").style.display="none";
          
   })
 
   // *****************************************FILTROS POR CATEGORIAS Y BOTONES*****************************************
   //Boton organizacion A-Z
-
   let BotonOrganizarAz = document.getElementById("personajesAZ");
-      BotonOrganizarAz.addEventListener("click", az)
-  
+  BotonOrganizarAz.addEventListener("click", az);
+ 
+ 
   function az(){ 
+    
    let organizaAz=Data.sort((personajes1,personajes2)=>{
    return (personajes1.name < personajes2.name)? -1 :1})
+  //  console.log(JSON.parse(StringJson));
    document.getElementById("filtroAZ").innerHTML=JSON.stringify(organizaAz);
    document.getElementById("contenedorTarjetas").style.display = "none";
    document.getElementById("filtroAZ").style.display="block";
-  
-   
-   //console.log(organizaAz)
+   document.getElementById("SomosQ").style.display = "none"; 
     }
 
 
 //**********Filtro de genero/**********
+
+
   const generoData=[];
   
   for(let i=0;i < Data.length; i++){
   generoData.push(Data[i].gender);
    if (Data[i].gender=="Female") {
-   console.log("mujeres" )
     
+     console.log(generoData.indexOf("Female"))
     }
-  //  else if (Data[i].gender=="Male") {
-  //   console.log("Hombres")
-  //  } 
+ 
    }
 
-//  console.log(generoData)
+
 
 let BotonMujeres = document.getElementById("GeneroMujeres");
 BotonMujeres.addEventListener("click", filtergenderF)
 
 function filtergenderF(){
- let genderF=Data.filter(genero=>{
+   let genderF= Data.filter(genero=>{
   return genero.gender==="Female"
  })
- console.log(genderF)
+  console.log(genderF)
 }
 
 let BotonHombres = document.getElementById("GeneroHombres");
